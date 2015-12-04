df4292f1ec2ff287433ee86abfefbff9e1b0ef1e
Tony <tonyzeru@gmail.com>
2015-12-04 15:45:40 -0600

    added modifed version of enrutatec

---
+++ EnrutaTEC/src/main/groovy/proyecto/services/RoadMapService.groovy
@@ -0,0 +1,22 @@
+package proyecto.services
+
+import proyecto.model.Stop
+import proyecto.model.Route
+
+
+interface RoadMapService {
+
+    /**
+     * <code>batch.cities[0].name</code>
+     * <code>batch.roads[0].from</code>
+     * <code>batch.roads[0].to</code>
+     * <code>batch.roads[0].name</code>
+     * <code>batch.roads[0].distanceKms</code>
+     *
+     * @param batch
+     */
+    void saveBatch(Map batch)
+
+    List<Stop> getStops()
+    List<Route> getRoutes()
+}
