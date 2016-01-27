40474a44f0ca429bb9f6b8a5f34d66f5e1130ce1
Marco <marco@marco-K53SV>
2016-01-26 22:48:05 -0600

    Updating frontend

---
+++ Enrutatec/src/main/java/com/enrutatec/services/RoutesManagerService.java
@@ -0,0 +1,18 @@
+package com.enrutatec.services;
+
+
+import com.enrutatec.model.Stop;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.enrutatec.application.MainActivity;
+import com.enrutatec.model.Route;
+
+import java.util.List;
+
+public interface RoutesManagerService {
+    List<Route> getRoutes();
+    void setRouteInfo(Route route, MainActivity activity);
+    void showRouteInfo(MainActivity activity, String name, double duration, long price, double distance);
+    double calcDistance(List<LatLng> coordinates);
+    void addCoordinate(Route route, LatLng coordinate);
+    Stop addStop(LatLng stop);
+}
